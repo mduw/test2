4000fc1776450de01a10e9a0ad6a9671fc635ccd
mitchelldang <mlcdang@uw.edu>
2021-02-14 17:07:41 -0800

    update

--- src/App.js
+++ src/App.js
@@ -1,42 +1,19 @@
-import React, { Component } from 'react';
-import Table from './Table';
+import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+
 import Form from './Form';
 
-class App extends Component {
-    state = {
-        characters: []
-    };
 
-    removeCharacter = index => {
-        const { characters } = this.state;
-    
-        this.setState({
-            characters: characters.filter((character, i) => { 
-                return i !== index;
-            })
-        });
-    }
 
-    handleSubmit = character => {
-        this.setState({characters: [...this.state.characters, character]});
-    }
+const App = () => (
+    <BrowserRouter>
+        <Switch>
+          <Route exact path="/">
+            <Form/>
+          </Route>
+        </Switch>
 
-    render() {
-        const { characters } = this.state;
-        
-        return (
-            <div className="container">
-                <h1>React Tutorial</h1>
-                <p>Add a character with a name and a job to the table.</p>
-                <Table
-                    characterData={characters}
-                    removeCharacter={this.removeCharacter}
-                />
-                <h3>Add New</h3>
-                <Form handleSubmit={this.handleSubmit} />
-            </div>
-        );
-    }
-}
-
-export default App;
+    </BrowserRouter>
+  );
+  
+  export default App;
